9b2a65d50e5d83ae3a7649e1f261ba03e8f6b3f2
amatheo <59826260+amatheo@users.noreply.github.com>
2023-05-02 11:12:44 +0200

    Clean up unused field

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,17 +5,14 @@ public class Portal : MonoBehaviour
 {
     [SerializeField] private Transform _spawnTo;
 
-    [SerializeField] private Collider2D _collider2D;
-    
     /**
      * On trigger enter 2D
      */
     public void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("OnTriggerEnter2D");
         if (other.tag == "Player")
         {
-            Debug.Log("trigger entered player");
+            Debug.Log("Teleporting player to " + _spawnTo.position);
             other.transform.position = _spawnTo.position;
         }
     }
