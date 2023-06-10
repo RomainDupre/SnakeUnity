cdf86642e705961fc54ebee46bb1d69ba5bb187c
Xan <xanmaris.pro@gmail.com>
2023-06-10 14:52:08 +0200

    Ajout du menu fonctionnel

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private int currentLevel = 1;
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(currentLevel);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void SelectionnerLevel1()
+    {
+        this.currentLevel = 1;
+    }
+
+    public void SelectionnerLevel2()
+    {
+        this.currentLevel = 2;
+    }
+
+    public void SelectionnerLevel3()
+    {
+        this.currentLevel = 3;
+    }
+
+}
